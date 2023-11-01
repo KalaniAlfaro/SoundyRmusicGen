4c255af55b32408fb66229d64666259b6751175a
KalaniAlfaro <96454534+KalaniAlfaro@users.noreply.github.com>
2023-11-01 19:06:25 -0300

    gtrjgyfjh

--- musicfile.py
+++ musicfile.py
@@ -2,9 +2,18 @@ from gradio_client import Client
 import streamlit as st
 
 
+
+params = st.experimental_get_query_params()
+# Obtém o valor do parâmetro 'variavel' da URL
+palavraChave = params.get('palavrachave', ['cavalo'])[0]
+
+# Mostra o valor recebido na interface do Streamlit
+st.write("Valor da variável via GET:", palavraChave)
+
+
 client = Client("https://facebook-musicgen.hf.space/")
 result = client.predict(
-				"Howdy!",	# str  in 'Describe your music' Textbox component
+				palavraChave,	# str  in 'Describe your music' Textbox component
 				"https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav",	# str (filepath or URL to file) in 'File' Audio component
 				fn_index=0
 )
