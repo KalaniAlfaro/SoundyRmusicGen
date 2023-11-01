d7a71e5f49ef0a865c0238d43ad4fa4fa8bd3b83
KalaniAlfaro <96454534+KalaniAlfaro@users.noreply.github.com>
2023-11-01 19:02:03 -0300

    staudio

--- musicfile.py
+++ musicfile.py
@@ -10,16 +10,19 @@ result = client.predict(
 )
 st.write(result)
 
-from base64 import b64encode
-from pathlib import Path
-from streamlit_player import st_player
+import numpy as np
 
-def local_video(path, mime="video/mp4"):
-    data = b64encode(Path(path).read_bytes()).decode()
-    return [{"type": mime, "src": f"data:{mime};base64,{data}"}]
+audio_file = open(result, 'rb')
+audio_bytes = audio_file.read()
 
-st_player(local_video(result))
+st.audio(audio_bytes, format='audio/mp4')
 
+sample_rate = 44100  # 44100 samples per second
+seconds = 2  # Note duration of 2 seconds
+frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
 
-
-st_player(result)
+st.audio(note_la, sample_rate=sample_rate)
