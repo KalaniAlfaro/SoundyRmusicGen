6103fcc539cbefcd99ad065a50efc47d57118a76
KalaniAlfaro <96454534+KalaniAlfaro@users.noreply.github.com>
2023-11-01 18:39:25 -0300

    player

---
+++ musicfile.py
@@ -0,0 +1,10 @@
+from gradio_client import Client
+import streamlit as st
+
+client = Client("https://facebook-musicgen.hf.space/")
+result = client.predict(
+				"Howdy!",	# str  in 'Describe your music' Textbox component
+				"https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav",	# str (filepath or URL to file) in 'File' Audio component
+				fn_index=0
+)
+st.write(result)
