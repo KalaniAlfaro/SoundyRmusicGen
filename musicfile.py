4118124a1f4f97cc07658dae40b82ca1272646f0
KalaniAlfaro <96454534+KalaniAlfaro@users.noreply.github.com>
2023-11-14 04:05:52 -0300

    sem bibliotecas

--- musicfile.py
+++ musicfile.py
@@ -2,8 +2,8 @@
 
 
 ###################################################################
-from gradio_client import Client
-import streamlit as st
-import os
+#from gradio_client import Client
+#import streamlit as st
+#import os
 $cavalo = "cavalo"
 echo $cavalo;
