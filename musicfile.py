ef7b396a2a8db543b4da334cd7fcd7972cbdf86e
KalaniAlfaro <96454534+KalaniAlfaro@users.noreply.github.com>
2023-11-14 05:08:09 -0300

    original form

--- musicfile.py
+++ musicfile.py
@@ -5,28 +5,40 @@
 from gradio_client import Client
 import streamlit as st
 import os
-st.write("Hello, World!")
 
-from gradio_client import Client
-import streamlit as st
-
-
-
-params = st.experimental_get_query_params()
-# Obtém o valor do parâmetro 'variavel' da URL
-palavraChave = params.get('palavrachave', ['cavalo'])[0]
-
-# Mostra o valor recebido na interface do Streamlit
-st.write("Valor da variável via GET:", palavraChave)
-
-from gradio_client import Client
-
-client = Client("https://facebook-musicgen.hf.space/")
+client = Client("https://huggingface.co/spaces/facebook/MusicGen?duplicate=true")
 result = client.predict(
-				"Howdy!",	# str  in 'parameter_16' Dataset component
-				fn_index=2
+				palavraChave,	# str  in 'Describe your music' Textbox component
+				"https://github.com/gradio-app/gradio/raw/main/test/test_files/audio_sample.wav",	# str (filepath or URL to file) in 'File' Audio component
+				fn_index=0
 )
-print(result)
+#nome e PATH do mp4
+st.write(result)
 
 
+audio_file = open(result, 'rb')
+audio_bytes = audio_file.read()
 
+#player de audio sem autoplay
+#st.audio(audio_bytes, format='audio/mp4')
+
+def autoplay_audio(file_path: str):
+    import base64
+
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio autoplay="true" controls class="stAudio" style="width:100%">
+            <source src="data:audio/mp4;base64,{b64}" type="audio/mp4">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
+#st.write("# Auto-playing Audio!")
+
+autoplay_audio(result)
