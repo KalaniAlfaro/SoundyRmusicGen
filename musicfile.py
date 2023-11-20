2af0729c5f6aa27bf32c7c954781590a2fcdc3d1
KalaniAlfaro <96454534+KalaniAlfaro@users.noreply.github.com>
2023-11-20 00:36:02 -0300

    aaaaaaaa

--- musicfile.py
+++ musicfile.py
@@ -13,7 +13,7 @@ params = st.experimental_get_query_params()
 palavraChave = params.get('palavrachave', ['cavalo'])[0]
 
 # Mostra o valor recebido na interface do Streamlit
-##st.write("Valor da variável via GET:", palavraChave)
+st.write("Valor da variável via GET:", palavraChave)
 
 
 client = Client("https://facebook-musicgen.hf.space/")
@@ -30,7 +30,7 @@ audio_file = open(result[0], 'rb')
 audio_bytes = audio_file.read()
 
 #player de audio sem autoplay
-##st.audio(audio_bytes, format='audio/mp4')
+st.audio(audio_bytes, format='audio/mp4')
 
 def autoplay_audio(file_path: str):
     import base64
